c630f08efe69a30712702b0fa748396ee7dc72ba
Neo0012 <nel_gonzales@hotmail.com>
2024-11-05 19:51:42 -0500

    Primer commit

---
+++ 01_HOLA_MUNDO/NELSON_GONZALES/CLASE/hola_mundo.dart
@@ -0,0 +1,10 @@
+///Hola mundo en Dart
+///main() -> función princiapal
+///{ -> bloque de la función o cuerpo
+///
+/// }
+
+void main() {
+  ///print es una función u objeto que imprime un valor por consola
+  print("Hola mudo con Dart");
+}
