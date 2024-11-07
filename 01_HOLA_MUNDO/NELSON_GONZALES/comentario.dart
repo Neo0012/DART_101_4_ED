7c70f98caa211aa5c2e496c5d6b5a14f59d0ab1e
Neo0012 <nel_gonzales@hotmail.com>
2024-11-06 19:05:13 -0500

    Segundo commit

---
+++ 01_HOLA_MUNDO/NELSON_GONZALES/comentario.dart
@@ -0,0 +1,12 @@
+void main() {
+  //comenmtario simple
+  /*
+  print("Hola mundo");
+  print("Hola mundo");
+  print("Hola mundo");
+  */
+ /// --> comentario de documentación print("Hola mundo");
+  print("Hola mundo");
+
+
+}
---
+++ 02_VARIABLES/00_RESOURCES/CLASE/variables.dart
@@ -0,0 +1,30 @@
+//Esat es una delcaración de una variable global
+String global = "Esta es una variable global";
+
+void main() {
+  //var
+  var nombre = "Nelson";
+
+  // late 
+  // inicializar mas tarde
+  late String edad = "14";
+
+  edad = "25";
+
+  print(edad);
+
+  //Recomendable que siempre definas la variable con el tipo de dao
+  String apellido = "Gonzales";
+
+  ///const y finla
+  ///
+  ///const -> se asigna su espacio de memoria al momento de compilar
+  ///
+  ///finla -> se asigna su espacio de memoria al ejecutar
+  ///
+  const velosidadSonido = 250;
+
+  final DiaSemana = "Lunes";
+
+
+}
