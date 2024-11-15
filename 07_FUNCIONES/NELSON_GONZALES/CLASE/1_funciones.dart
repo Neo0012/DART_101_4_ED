ff013562a8c5784acef9dc11b5871f85d6e386dd
Neo0012 <nel_gonzales@hotmail.com>
2024-11-14 20:24:21 -0500

    add: funciones

--- 07_FUNCIONES/NELSON_GONZALES/CLASE/1_funciones.dart
+++ 07_FUNCIONES/NELSON_GONZALES/CLASE/1_funciones.dart
@@ -1,4 +1,14 @@
 // Funciones principales
 void main() {
-  
+  // Tuplas
+(double, double, String, bool) funcionTupla() {
+  return (1.0, 2.0, "Hola", true);
 }
+final valorTupla = funcionTupla();
+  print("Valor 1: ${valorTupla.$3}");
+
+  // Tip - asignar variables descriptivas 
+  final (valorTupla1, valorTupla2, valorTupla3, valorTupla4) = funcionTupla();
+  print(valorTupla2);
+}
+
---
+++ 07_FUNCIONES/NELSON_GONZALES/CLASE/2_parametros_posicionales.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+}
+
+// Argumentos de la fución
+// Argumentos por posición
+// Argumentos por nombre
+// todo parametro dentro de los parentecis
+// Parametro 
+
+double suma() {
+  return 2 + 1;
+}
