9288cd04c79e24af36f07dacff8ac865fa716198
Neo0012 <nel_gonzales@hotmail.com>
2024-11-19 21:19:56 -0500

    add:clases

--- 08_POO/NELSON_GONZALES/CLASE/5_callback_parametros.dart
+++ 08_POO/NELSON_GONZALES/CLASE/5_callback_parametros.dart
@@ -1,8 +1,16 @@
 void main() {
   diaDeLaSemana(
     dia: "Lunes",
-    callbackParametros: (valor, valor2, valor3){
+    callbackParametros: (valor, valor2, valor3) {
       print(valor);
+      return "Algo más";
+    },
+  );
+
+  diaDeLaSemana(
+    dia: "Viernes",
+    callbackParametros: (value1, value2, value3) {
+      return "Hola mundo";
     },
   );
 }
@@ -11,11 +19,18 @@ void diaDeLaSemana({
   required String dia,
   // Callback se declara -> Function
   // Tipo de dato -> Function(String)
-  required Function(String, double, int) callbackParametros,
+  required String Function(String, double, int) callbackParametros,
 }) {
   // Cuando ejecutamos un callback podemos envar valores desde el callback
   final valorProcesado = "El día de la semana es: $dia";
 
   // Vimos anteriormente -> callbackParametros(); -> callbackParametros.call();
-  callbackParametros.call(valorProcesado, 10.0, 10);
+  final variable = callbackParametros.call(valorProcesado, 10.0, 10);
+  print("En la función la variable es: $variable");
+
+  // final mensaje = saludo();
 }
+
+// String saludo() {
+//   return "Hola...";
+// }
